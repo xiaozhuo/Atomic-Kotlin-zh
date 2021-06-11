3432dcdf08bfd668b277bfc55331a0c89cb525ad
liuanguang <liuanguang@cvte.com>
2021-06-11 10:06:37 +0800

    build: update vuepress config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -27,71 +27,75 @@ module.exports = {
         selectText: '选择语言',
         label: '简体中文',
         nav: [
-          { text: '目录', link: 'Table-of-Contens' },
+          { text: '目录', link: '/Table-of-Contens' },
           { text: '官网', link: 'https://www.atomickotlin.com' },
         ],
         editLinkText: '翻译有错？帮助改进:)',
         // 侧边栏配置
-        sidebar: [
-          'Copyright',
-          {
-            title: 'Section 1',
-            path: 'Section-1',
-            collapsable: false,
-            children: [
-              'Chapter-1',
-              'Chapter-2',
-              'Chapter-3',
-              'Chapter-4',
-              'Chapter-5',
-              'Chapter-6',
-              'Chapter-7',
-              'Chapter-8',
-              'Chapter-9',
-              'Chapter-10',
-              'Chapter-11',
-              'Chapter-12',
-              'Chapter-13',
-              'Chapter-14',
-              'Chapter-15',
-            ]
-          },
-        ]
+        sidebar: {
+          '/': [
+            'Copyright',
+            {
+              title: 'Section 1',
+              path: 'Section-1',
+              collapsable: false,
+              children: [
+                'Chapter-1',
+                'Chapter-2',
+                'Chapter-3',
+                'Chapter-4',
+                'Chapter-5',
+                'Chapter-6',
+                'Chapter-7',
+                'Chapter-8',
+                'Chapter-9',
+                'Chapter-10',
+                'Chapter-11',
+                'Chapter-12',
+                'Chapter-13',
+                'Chapter-14',
+                'Chapter-15',
+              ]
+            },
+          ]
+        }
       },
       '/en/': {
         selectText: 'Languages',
         label: 'English',
         ariaLabel: 'Languages',
         nav: [
-          { text: 'Catalog', link: 'Table-of-Contens' },
+          { text: 'Catalog', link: '/en/Table-of-Contens' },
           { text: 'Official', link: 'https://www.atomickotlin.com' },
         ],
         editLinkText: 'Edit this page on GitHub :)',
-        sidebar: [
-          'Copyright',
-          {
-            title: 'Section 1',
-            path: 'Section-1',
-            collapsable: false,
-            children: [
-              'Chapter-1',
-              'Chapter-2',
-              'Chapter-3',
-              'Chapter-4',
-              'Chapter-5',
-              'Chapter-6',
-              'Chapter-7',
-              'Chapter-8',
-              'Chapter-9',
-              'Chapter-10',
-              'Chapter-11',
-              'Chapter-12',
-              'Chapter-13',
-              'Chapter-14',
-              'Chapter-15',
-            ]
-          }
-        ]
+        sidebar: {
+          '/en/': [
+            'Copyright',
+            {
+              title: 'Section 1',
+              path: 'Section-1',
+              collapsable: false,
+              children: [
+                'Chapter-1',
+                'Chapter-2',
+                'Chapter-3',
+                'Chapter-4',
+                'Chapter-5',
+                'Chapter-6',
+                'Chapter-7',
+                'Chapter-8',
+                'Chapter-9',
+                'Chapter-10',
+                'Chapter-11',
+                'Chapter-12',
+                'Chapter-13',
+                'Chapter-14',
+                'Chapter-15',
+              ]
+            }
+          ]
+        }
       }
     },
   },
